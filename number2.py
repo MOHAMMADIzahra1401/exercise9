fae20218b9fab265dd2c153942574212ae3beb21
MOHAMMADIzahra1401 <140957622+MOHAMMADIzahra1401@users.noreply.github.com>
2023-08-07 10:45:56 -0700

    Add files via upload

---
+++ number2.py
@@ -0,0 +1,21 @@
+import math
+def moadele2():
+
+    a=int(input("Enter a for moadele daraje 2:"))
+    b=int(input("Enter b for moadele daraje 2:"))
+    c=int(input("Enter c for moadele daraje 2:"))
+    #ax^2+bx+c
+    
+    delta=(b**2)-(4*(a*c))
+    print(delta)
+    if delta>0:
+        result1=(-b+math.sqrt(delta))/(2*a)
+        result2=(-b-math.sqrt(delta))/(2*a)
+        print(result1)
+        print(result2)
+    elif delta==0:
+        result3=(-1*b)/(2*a)
+        print(result3)
+    elif delta<0:
+        print("in moadele daraje 2 hal nemishvad")
+moadele2()
